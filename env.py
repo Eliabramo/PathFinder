62cf474dbd62f138c8e24208eae79ed4242a41d1
eliabramo <eliabramo@gmail.com>
2017-04-25 07:20:30 +0300

    balls angle fix

--- env.py
+++ env.py
@@ -5,6 +5,7 @@ import random
 red = (255,0,0)
 green = (0,255,0)
 blue =(0,0,255)
+white =(255,255,255)
 
 class ball():
     def __init__(self,env_width,env_height):
@@ -13,7 +14,7 @@ class ball():
         self.head = random.randint(0,359)
         self.width = env_width
         self.height = env_height
-        self.position = random.randint(0,self.width-1),random.randint(0,self.height-1)
+        self.position = random.randint(self.radius,self.width-self.radius-1),random.randint(self.radius,self.height-self.radius-1)
 
     def step(self,turn):
         self.head = self.head+turn
@@ -31,25 +32,52 @@ class ball():
 
 class env():
     def __init__(self):
-        self.num_enemies = 100
+        self.num_enemies = 10
         self.width = 800
         self.height = 400
         self.screen = np.zeros((self.height,self.width,3),np.uint8)
         self.red_balls = [ball(self.width,self.height) for b in range(self.num_enemies)]
+        self.green_ball = ball(self.width,self.height)
+        self.blue_ball = ball(self.width, self.height)
         self.state = 0
         self.reward = 0
+        self.view_angles = [-20,-10,0,10,20]
+        self.view_len = 10
 
     def reset(self):
         return self.state
 
-    def step(self,a):
+    def step(self, action):
+        ''' take one step of the envirment, move red and green balls and calculate reward 
+            action: blue ball head angle, between -30 and 30 '''
+
+        # draw red and green balls on the screen
         self.screen = np.zeros((self.height, self.width, 3), np.uint8)
         for b in self.red_balls:
-            pos = b.step(0)
+            b.step(0)
             cv2.circle(self.screen, b.position, b.radius, red, -1)
-        return self.state, self.reward
+        cv2.circle(self.screen, self.green_ball.position, self.green_ball.radius, green, -1)
+
+        # move blue ball
+        pos = self.blue_ball.step(action)
+        cv2.circle(self.screen, self.blue_ball.position, self.blue_ball.radius, blue, -1)
+
+        # calculate the reward
+
+        return self.state, self.view_angleself.reward
 
     def render(self):
+        ''' draw the screen for user observation '''
+        # draw viewing vectors on the screen
+        p = self.blue_ball.position
+        r = self.blue_ball.radius
+        for ang in self.view_angles:
+            dx = np.cos(ang * np.pi / 180)
+            dy = np.sin(ang * np.pi / 180)
+            for i in range(self.view_len):
+                self.screen[int(dx)]
+
+
         cv2.imshow('screen', self.screen[:,:,::-1])
         cv2.waitKey(1)
         #test
@@ -68,5 +96,6 @@ if __name__ == '__main__':
     # cv2.waitKey(-1)
     e = env()
     for i in range(10000):
-       e.step(0)
+       head = random.randint(-30,30)
+       e.step(head)
        e.render()
